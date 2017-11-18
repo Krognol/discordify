6707227477d117ede7fd4bac84dfb4a3ffd857a8
Krognol <timgabe@gmail.com>
2017-11-18 17:44:07 +0100

    Add project files.

---
+++ discordify/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace discordify {
+	partial class Form1 {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+			this.discordConnectButton = new System.Windows.Forms.Button();
+			this.discordDisconnectButton = new System.Windows.Forms.Button();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.statusStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel2});
+			this.statusStrip1.Location = new System.Drawing.Point(0, 51);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(434, 22);
+			this.statusStrip1.SizingGrip = false;
+			this.statusStrip1.TabIndex = 4;
+			this.statusStrip1.Text = "ASdf";
+			// 
+			// toolStripStatusLabel2
+			// 
+			this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+			this.toolStripStatusLabel2.Size = new System.Drawing.Size(38, 17);
+			this.toolStripStatusLabel2.Text = "Hello!";
+			// 
+			// toolStripStatusLabel1
+			// 
+			this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+			this.toolStripStatusLabel1.Size = new System.Drawing.Size(0, 17);
+			// 
+			// discordConnectButton
+			// 
+			this.discordConnectButton.Location = new System.Drawing.Point(12, 12);
+			this.discordConnectButton.Name = "discordConnectButton";
+			this.discordConnectButton.Size = new System.Drawing.Size(75, 23);
+			this.discordConnectButton.TabIndex = 2;
+			this.discordConnectButton.Text = "Connect";
+			this.discordConnectButton.UseVisualStyleBackColor = true;
+			this.discordConnectButton.Click += new System.EventHandler(this.discordConnectButton_Click);
+			// 
+			// discordDisconnectButton
+			// 
+			this.discordDisconnectButton.Location = new System.Drawing.Point(347, 12);
+			this.discordDisconnectButton.Name = "discordDisconnectButton";
+			this.discordDisconnectButton.Size = new System.Drawing.Size(75, 23);
+			this.discordDisconnectButton.TabIndex = 3;
+			this.discordDisconnectButton.Text = "Disconnect";
+			this.discordDisconnectButton.UseVisualStyleBackColor = true;
+			this.discordDisconnectButton.Click += new System.EventHandler(this.discordDisconnectButton_Click);
+			// 
+			// timer1
+			// 
+			this.timer1.Enabled = true;
+			this.timer1.Interval = 10000;
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(434, 73);
+			this.Controls.Add(this.discordDisconnectButton);
+			this.Controls.Add(this.discordConnectButton);
+			this.Controls.Add(this.statusStrip1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+			this.MaximizeBox = false;
+			this.Name = "Form1";
+			this.ShowIcon = false;
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+			this.Text = "Discordify";
+			this.statusStrip1.ResumeLayout(false);
+			this.statusStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.Button discordConnectButton;
+		private System.Windows.Forms.Button discordDisconnectButton;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+		private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+		private System.Windows.Forms.Timer timer1;
+	}
+}
+
---
+++ discordify/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Linq;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using spotify;
+using discordrpc;
+
+namespace discordify {
+	internal enum PlayingStatus {
+		Playing,
+		Paused,
+	}
+
+	internal struct Status {
+		public string CurArtist;
+		public string CurTrack;
+		public PlayingStatus PlayStatus;
+	}
+
+	public partial class Form1 : Form {
+		private static Spotify spotifyClient;
+
+		private const int WH_KEYBOARD_LL = 13;
+		private const int WM_KEYDOWN = 0x0100;
+		private static LowLevelKeyboardProc _proc = HookCallback;
+		private static IntPtr _hookID = IntPtr.Zero;
+
+		private const int MEDIA_NEXT = 0xB0;
+		private const int MEDIA_PREV = 0xB1;
+		private const int MEDIA_PAPL = 0xB3;
+		private const int MEDIA_MUTE = 0xAD;
+
+		private const string DISCORD_CLIENT_ID = "YOUR_DISCORD_CLIENT_ID";
+		private const string SPOTIFY_CLIENT_ID = "YOUR_SPOTIFY_CLIENT_ID";
+		private const string SPOTIFY_CLIENT_SECRET = "YOUR_SPOTIFY_CLIENT_SECRET";
+
+		private static DiscordRpc.RichPresence presence;
+		private static DiscordRpc.EventHandlers handlers;
+		private static Status curStatus;
+		private static DateTime lastSwitch = DateTime.UtcNow;
+
+		public Form1() {
+			InitializeComponent();
+
+			// "omit" the args since we won't actually use them
+			FormClosing += (object _, FormClosingEventArgs __) => DiscordRpc.Shutdown();
+
+			// Initialize the Spotify client
+			spotifyClient = new Spotify(SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET);
+
+			// These won't change if your application is set up properly 
+			// so init them at the start
+			presence.largeImageKey = "spotify_large";
+			presence.smallImageKey = "spotify_small";
+
+			curStatus = new Status {
+				CurArtist = "",
+				CurTrack = "",
+				PlayStatus = PlayingStatus.Paused
+			};
+
+			// We run Show() here since Application.Run() will hijack the thread 
+			// and won't show the window otherwise
+			Show();
+
+			// Set the hook for intercepting the media key presses
+			_hookID = SetHook(_proc);
+			Application.Run();
+			UnhookWindowsHookEx(_hookID);
+		}
+
+		// Call DiscordRpc.Shutdown when clicking disconnect button
+		// This is probably redundant though since we call Shutdown
+		// on FormClosing
+		private void discordDisconnectButton_Click(object sender, EventArgs e) => DiscordRpc.Shutdown();
+
+		// DiscordRpc Ready callback
+		private void Ready() => toolStripStatusLabel2.Text = "Ready!";
+
+		// DiscordRpc Disconnected callback
+		private void Disconnected(int errorCode, string message) => toolStripStatusLabel2.Text = string.Format("Error {0}: {1}", errorCode, message);
+
+		// DiscordRpc Error callback
+		private void Error(int errorCode, string message) => toolStripStatusLabel2.Text = string.Format("Error {0}: {1}", errorCode, message);
+
+		// Connect button handler
+		private void discordConnectButton_Click(object sender, EventArgs e) {
+			// Initialize the event handlers and DiscordRpc so we can begin
+			// updating our rich presence
+			handlers = new DiscordRpc.EventHandlers {
+				readyCallback = Ready,
+			};
+
+			handlers.disconnectedCallback += Disconnected;
+			handlers.errorCallback += Error;
+
+			DiscordRpc.Initialize(DISCORD_CLIENT_ID, ref handlers, true, null);
+			DiscordRpc.RunCallbacks();
+		}
+
+		private static async void UpdatePresence() {
+			string[] splitChar = { " - " };
+			string track, artist = "";
+
+			// The process that has the currently playing track seems to change every once in a while for some reason
+			using (var proc = (from p in Process.GetProcessesByName("Spotify") where p.MainWindowTitle != "" select p).FirstOrDefault()) {
+				// Split the artist and track name
+				var titleSplit = proc.MainWindowTitle.Split(splitChar, 2, StringSplitOptions.None);
+				artist = titleSplit[0];
+				track = titleSplit[1];
+			}
+
+			// Check so the current artist and track aren't the same as
+			if (curStatus.CurArtist == artist && curStatus.CurTrack == track) return;
+
+			curStatus.CurArtist = artist;
+			curStatus.CurTrack = track;
+
+			// Search for the track via spotify
+			var spotifyStuff = await spotifyClient.SearchTrack(Uri.EscapeDataString($"artist:{artist} {track}"));
+			Spotify.Tracks? tracks = null;
+			if (spotifyStuff.HasValue) tracks = spotifyStuff?.Tracks;
+
+			// Update the presence if the track was found
+			if (tracks != null && tracks?.Items.Count > 0) {
+				presence.details = $"Listening to {tracks?.Items[0].Name} by {tracks?.Items[0].Artists[0].Name}";
+
+				var now = DateTime.UtcNow;
+				presence.startTimestamp = DateTimeToTimestamp(now);
+				presence.endTimestamp = DateTimeToTimestamp(now.AddMilliseconds((int)tracks?.Items[0].DurationMs));
+
+				DiscordRpc.UpdatePresence(ref presence);
+			}
+		}
+
+		// Every 10 seconds we update the presence
+		private void timer1_Tick(object sender, EventArgs e) => UpdatePresence();
+
+		// Convert the Utc date time to a valid timestamp
+		private static long DateTimeToTimestamp(DateTime dt) => (dt.Ticks - 621355968000000000) / 10000000;
+
+		// Key intercepting stuff
+
+		private static IntPtr SetHook(LowLevelKeyboardProc proc) {
+			using (var curProc = Process.GetCurrentProcess())
+			using (var curModule = curProc.MainModule) {
+				return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+			}
+		}
+
+		private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+		private static IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam) {
+			if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN) {
+				var vkCode = Marshal.ReadInt32(lParam);
+				if (vkCode == MEDIA_PAPL) {
+					// Paused/Playing can be used to change the presence details
+					// or the timestamp
+					switch (curStatus.PlayStatus) {
+					case PlayingStatus.Paused:
+						curStatus.PlayStatus = PlayingStatus.Playing;
+						break;
+					case PlayingStatus.Playing:
+						curStatus.PlayStatus = PlayingStatus.Paused;
+						break;
+					}
+					UpdatePresence();
+				} else if (vkCode == MEDIA_NEXT || vkCode == MEDIA_PREV) {
+					// If it's been more than 5 seconds since the last track switch
+					// we can update the presence again
+					if (DateTime.UtcNow.Subtract(lastSwitch) >= TimeSpan.FromSeconds(5)) {
+						lastSwitch = DateTime.UtcNow;
+						UpdatePresence();
+					}
+				}
+			}
+			return CallNextHookEx(_hookID, nCode, wParam, lParam);
+		}
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		private static extern IntPtr GetModuleHandle(string lpModuleName);
+	}
+}
